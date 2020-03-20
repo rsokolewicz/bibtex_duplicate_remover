6ff04a8187137920f1fbf78de321926293f89bb7
rsokolewicz <rsokolewicz@gmail.com>
2020-03-20 14:24:22 +0100

    updated readme

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 def readme():
-    with open('README.rst') as f:
+    with open('README.md') as f:
         return f.read()
 
 setup(name='bibtex_duplicate_remover',
@@ -9,8 +9,7 @@ setup(name='bibtex_duplicate_remover',
       description='Removes duplicates in bibliography created by bibtex and biblatex',
       long_description='Removes duplicates in bibliography created by bibtex and biblatex and updates each cite key in tex files',
       url='http://github.com/rsokolewicz/bibtex_duplicate_remover',
-      classifiers = [ 'Programming Language :: Python :: 3.8',
-
+      classifiers = [ 'Programming Language :: Python :: 3.8' ],
       author='rsokolewicz',
       author_email='rsokolewicz@gmail.com',
       packages=['bibtex_duplicate_remover'],
